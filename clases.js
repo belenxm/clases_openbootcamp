35446b6f35a0001f8e2a8273b20bcee0ddfcd1b6
fer.belen <belenxmorales@gmail.com>
2023-07-05 15:40:45 -0300

    clases en javascript

---
+++ clases.js
@@ -0,0 +1,20 @@
+class Estudiante {
+    constructor(nombre){
+    this.nombre = nombre;
+    this.asignaturas = ['Javascript', 'HTML', 'CSS'];
+    
+}
+
+obtenDatos(){
+    return{
+        nombre : this.nombre,
+        asignaturas:this.asignaturas
+    };
+}
+}
+
+
+const estudiante = new Estudiante('Nombre del estudiante');
+const datosEstudiante = estudiante.obtenDatos();
+console.log(datosEstudiante);
+
